b374b38f4c6612d5c4c2a9874b5ab09c568656e8
rahmaulidyaa <rahma.maulidya98@gmail.com>
2022-05-29 23:50:03 +0700

    Update Challenge 2 - Logic Flow

--- index.js
+++ index.js
@@ -23,19 +23,33 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+  let hasilLooping = [];
+  for (const element of dataYangAkanDilooping) {
+    hasilLooping.push(element['namaDepan'] + ' ' + element['namaBelakang']);
+  }
+
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
   let jumlahPria = null;
+  for (const element of dataYangAkanDilooping) {
+    if (element['jenisKelamin'] == 'M') {
+      jumlahPria++;
+    }
+  }
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
   let jumlahWanita = null;
+  for (const element of dataYangAkanDilooping) {
+    if (element['jenisKelamin'] == 'F') {
+      jumlahWanita++;
+    }
+  }
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -49,6 +63,13 @@ function lakukanLooping(arrPegawai) {
         "Jumlah Pria dan Wanita berimbang"
   */
   let komentar = null;
+  if (jumlahPria > jumlahWanita) {
+    komentar = 'Jumlah Pria lebih banyak dari Wanita';
+  } else if (jumlahPria < jumlahWanita) {
+    komentar = 'Jumlah Wanita lebih banyak dari Pria';
+  } else {
+    komentar = 'Jumlah Pria dan Wanita berimbang';
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
